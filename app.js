eeb324d8650b889c8b4a55f8ada1ed09600a88ea
Justin Choi <justin.ichiro.choi@gmail.com>
2024-06-10 18:34:32 -0700

    Testing out Updating matches

--- app.js
+++ app.js
@@ -304,6 +304,40 @@ app.put('/put-trainer-ajax', function(req,res,next){
         console.log('test')
 })});
 
+app.put('/put-match-ajax', function(req,res,next){
+    let data = req.body;
+  
+    let homeworld = parseInt(data.homeworld);
+    let person = parseInt(data.fullname);
+  
+    let queryUpdateWorld = `UPDATE bsg_people SET homeworld = ? WHERE bsg_people.id = ?`;
+    let selectWorld = `SELECT * FROM bsg_planets WHERE id = ?`
+  
+          // Run the 1st query
+          db.pool.query(queryUpdateWorld, [homeworld, person], function(error, rows, fields){
+              if (error) {
+  
+              // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+              console.log(error);
+              res.sendStatus(400);
+              }
+  
+              // If there was no error, we run our second query and return that data so we can use it to update the people's
+              // table on the front-end
+              else
+              {
+                  // Run the second query
+                  db.pool.query(selectWorld, [homeworld], function(error, rows, fields) {
+  
+                      if (error) {
+                          console.log(error);
+                          res.sendStatus(400);
+                      } else {
+                          res.send(rows);
+                      }
+                  })
+              }
+  })});
 /*
     LISTENER
 */
--- public/js/update_match.js
+++ public/js/update_match.js
@@ -1,39 +1,46 @@
 // Get the objects we need to modify
-let updateTrainerForm = document.getElementById('update-trainer-form-ajax');
+let updatePersonForm = document.getElementById('update-person-form-ajax');
 
 // Modify the objects we need
-updateTrainerForm.addEventListener("submit", function (e) {
+updatePersonForm.addEventListener("submit", function (e) {
    
     // Prevent the form from submitting
     e.preventDefault();
 
+    // Get form fields we need to get data from
+    let inputFullName = document.getElementById("mySelect");
+    let inputHomeworld = document.getElementById("input-homeworld-update");
+
     // Get the values from the form fields
-    let matchIDValue = document.getElementById("mySelect").value;
-    let trainerPhoneValue = document.getElementById("input-edit-contestant-1").value;
-    let trainerEmailValue = document.getElementById("input-edit-contestant-2").value;
+    let fullNameValue = inputFullName.value;
+    let homeworldValue = inputHomeworld.value;
+    
+    // currently the database table for bsg_people does not allow updating values to NULL
+    // so we must abort if being bassed NULL for homeworld
+
+    if (isNaN(homeworldValue)) 
+    {
+        return;
+    }
 
 
     // Put our data we want to send in a javascript object
     let data = {
-        trainerID: trainerIDValue,
-        trainerPhone: trainerPhoneValue,
-        trainerEmail: trainerEmailValue,
-        numberOfWins: numberOfWinsValue,
+        fullname: fullNameValue,
+        homeworld: homeworldValue,
     }
     
     // Setup our AJAX request
     var xhttp = new XMLHttpRequest();
-    xhttp.open("PUT", "/put-trainer-ajax", true);
+    xhttp.open("PUT", "/put-person-ajax", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
-    
     // Tell our AJAX request how to resolve
     xhttp.onreadystatechange = () => {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
 
             // Add the new data to the table
-            console.log("Yes!")
-            updateRow(xhttp.response, trainerIDValue);
+            updateRow(xhttp.response, fullNameValue);
 
         }
         else if (xhttp.readyState == 4 && xhttp.status != 200) {
@@ -47,33 +54,24 @@ updateTrainerForm.addEventListener("submit", function (e) {
 })
 
 
-function updateRow(data, trainerID){
+function updateRow(data, personID){
     let parsedData = JSON.parse(data);
     
-    let table = document.getElementById("trainer-table");
+    let table = document.getElementById("people-table");
 
     for (let i = 0, row; row = table.rows[i]; i++) {
        //iterate through rows
        //rows would be accessed using the "row" variable assigned in the for loop
-       if (table.rows[i].getAttribute("data-value") == matchID) {
+       if (table.rows[i].getAttribute("data-value") == personID) {
 
             // Get the location of the row where we found the matching person ID
             let updateRowIndex = table.getElementsByTagName("tr")[i];
 
             // Get td of homeworld value
-            let phone_cell = updateRowIndex.getElementsByTagName("td")[2];
-            let email_cell = updateRowIndex.getElementsByTagName("td")[3];
-            let number_of_wins_cell = updateRowIndex.getElementsByTagName("td")[4];
-
-
+            let td = updateRowIndex.getElementsByTagName("td")[3];
 
             // Reassign homeworld to our value we updated to
-            email_cell.innerHTML = parsedData[0].trainerEmail; 
-            phone_cell.innerHTML = parsedData[0].trainerPhone; 
-            number_of_wins_cell.innerHTML = parsedData[0].numberOfWins; 
-
+            td.innerHTML = parsedData[0].name; 
        }
     }
-
-    location.reload();
 }
